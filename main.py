9f1876e9bf7cadd08c9be218418a79f3547c62e5
Mahmoud Bentriou <moud@MacBookPro.lan>
2025-08-11 00:23:35 +0200

    feat: Add arg parse in main

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+import argparse
+
 from arabic_lecture_cleaner.cleaner import Cleaner
 
 
-def main():
+def main(mode: str, model: str):
     cleaner = Cleaner(data_dir="./data")
-    cleaner.run()
+    cleaner.run(mode=mode, model=model)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        prog="arabic-lecture-cleaner",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--mode",
+        required=False,
+        default="local",
+        type=str,
+        help="Operating mode ('api' or 'local').",
+        choices=["api", "local"],
+    )
+    parser.add_argument(
+        "--model",
+        required=False,
+        default="mistral-small3.2:24b",
+        type=str,
+        help="Model identifier (depends on local or API call).",
+    )
+    args = parser.parse_args()
+
+    main(mode=args.mode, model=args.model)
